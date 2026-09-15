976d3937b43ed15590719f3d0bf5b698970e429a
Shreya Pawar Mamidi <shreyapawarmamidi@gmail.com>
2026-09-15 12:01:56 -0400

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        # you need to keep tack of the running sum to add
+        # keep track of the record itll be as long as the operations
+        # need a stack of all the numbers you got
+
+        record = []
+
+        for op in operations:
+            if op == "C":
+                record.pop()
+                
+            elif op == "D":
+                record.append(2 * record[-1])
+
+            elif op == "+":
+                record.append(record[-1]+ record[-2])
+            else: 
+                record.append(int(op))
+            print(record)
+
+        summ = 0
+        for num in record:
+            summ += num
+
+        return summ
+
+
+
+        
